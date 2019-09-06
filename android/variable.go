365f0e21e656e085cbd66b8191b7f453254e344c
Simon Shields <simon@lineageos.org>
2019-09-06 20:38:44 +0700

    soong: extend with du board flags
    Includes the following commit:
    
    Author: Simon Shields <simon@lineageos.org>
    Date:   Mon Sep 4 21:09:44 2017 +1000
    
        soong: explicitly specify name of Lineage variables struct
    
        Change-Id: Ic754b8a13658ddc40620f985581bd1754b780b15
    
    Change-Id: If127c9da6b57340ae57e0cf725151cdcc3c82ef3

--- android/variable.go
+++ android/variable.go
@@ -20,6 +20,8 @@ import (
 	"runtime"
 	"strings"
 
+	"du/soong/android"
+
 	"github.com/google/blueprint/proptools"
 )
 
@@ -114,6 +116,9 @@ type variableProperties struct {
 			Static_libs  []string
 			Srcs         []string
 		}
+
+		// include custom variables
+		Du android.Product_variables
 	} `android:"arch_variant"`
 }
 
@@ -230,6 +235,9 @@ type productVariables struct {
 	PgoAdditionalProfileDirs []string `json:",omitempty"`
 
 	VendorVars map[string]map[string]string `json:",omitempty"`
+
+	// include Custom variables
+	Du android.ProductVariables
 }
 
 func boolPtr(v bool) *bool {
